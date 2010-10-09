1c56c2fe24b2770dd807d0ba04d5d26107506958
Jonathan Groll <jjg@groll.co.za>
2010-10-09 15:09:56 +0200

    working mince script

--- mince.sh
+++ mince.sh
@@ -6,34 +6,27 @@ set -e
 
 CHUNK_SIZE=1000000
 SCRATCH_DIR=~/scratch_space
-TAR_REGEX='*tar*'
+TAR_REGEX='\.tar'
 
 usage() {
-echo "ERROR: "
-echo " $*"
-echo "USAGE: "
-echo " mince.sh DIRECTORY/ARCHIVE PUBLIC_KEY_NAME"
-echo "EXAMPLE: "
-echo " ./mince.sh directory 'myfriend@her.isp.net'"
-echo "FURTHER COMMENTS: "
-echo " if an ARCHIVE is supplied instead of a directory, it must have a name like file.tar or file.tar.gz or file.tar.bz2 "
+    echo "ERROR: "
+    echo " $*"
+    echo "USAGE: "
+    echo " mince.sh DIRECTORY/ARCHIVE PUBLIC_KEY_NAME"
+    echo "EXAMPLE: "
+    echo " ./mince.sh directory 'myfriend@her.isp.net'"
+    echo "FURTHER COMMENTS: "
+    echo " if an ARCHIVE is supplied instead of a directory, it must have a name like file.tar or file.tar.gz or file.tar.bz2 "
 }
 
-# find_files() {
-# find . -type f ! -name "*.pgp" ! -name .DS_Store ! -name "*.gpg" ! -name "*.sda.exe"
-# }
-
-[[ $1 =~ $TAR_REGEX ]] && echo "matches!"
-[[ ! $1 =~ $TAR_REGEX ]] && echo "does not match"
-
 #check parameters entered are valid
-[ $# -ne 1 ] && usage "Missing required directory or archive argument." && exit 1
- if [ ! -d "$1" ] && [[ ! $1 =~ $TAR_REGEX ]]; then
-  usage "$1 is not a directory or tar/tar.gz/tar.bz2 archive."
-  exit 1
- fi
+[ $# -ne 2 ] && usage "Two parameters are required." && exit 1
+if [ ! -d "$1" ] && [[ ! $1 =~ $TAR_REGEX ]]; then
+    usage "$1 is not a directory or tar/tar.gz/tar.bz2 archive."
+    exit 1
+fi
 
-#if it is a directory, then tar it up in the scratch space
+#if 1st parameter is a directory, then tar it up in the scratch space
 if [ -d "$1" ]; then
     absolute="`cd $1; pwd` "
     mkdir -p $SCRATCH_DIR
@@ -43,25 +36,32 @@ if [ -d "$1" ]; then
     created=true
     echo "Created temporary archive $arch"
 else
-  arch=$1
-  created=false
-  echo "Working with archive $arch"
+    arch="`readlink -f $1`"
+    created=false
+    echo "Working with archive $arch"
 fi
 
 #call for GPG encryption and compression of the archive
-target="${SCRATCH_DIR}/${1}.gpg"
-gpg --encrypt --recipient 'myfriend@his.isp.net' foo.txt
+target="${arch##*/}.gpg"
+cd $SCRATCH_DIR
+gpg --bzip2-compress-level 6 --compress-algo bzip2 --output $target --encrypt --recipient $2 $arch
 
+#split .gpg file into chunks of size CHUNK_SIZE
+outdir="${SCRATCH_DIR}/output"
+mkdir -p outdir
+cd outdir
+split -b $CHUNK_SIZE ../$target
+for x in *
+do
+    mv $x "${1}__$x"
+done
 
-echo "State of created flag: $created"
+#clean up - remove .gpg and temporary archive
+cd $SCRATCH_DIR
+rm $target
+if [ $created == true ]; then
+    echo "Removing temporary archive $arch"
+    rm $arch
+fi
 
-# cd "$1"
-
-# find_files | while read file ; do
-# [ -e "$file.gpg" ] && rm -f "$file.gpg"
-# echo "Encrypting: $file"
-# gpg -u user@domain.ext -r user@domain.ext --sign --encrypt "$file"
-# echo " Created: $file.gpg"
-# echo " Removing: $file"
-# rm -f "$file"
-# done
+echo "All file splits produced placed in $outdir"
